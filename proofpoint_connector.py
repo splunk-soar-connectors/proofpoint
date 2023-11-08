5f5a54cd7d93175a3da33c217a1d9c560f7fb42b
mishalp-crest <mishalp@splunk.com>
2023-11-08 11:38:19 +0530

    minor changes

--- proofpoint_connector.py
+++ proofpoint_connector.py
@@ -589,6 +589,7 @@ if __name__ == '__main__':
     argparser.add_argument('input_test_json', help='Input Test JSON file')
     argparser.add_argument('-u', '--username', help='username', required=False)
     argparser.add_argument('-p', '--password', help='password', required=False)
+    argparser.add_argument('-v', '--verify', action='store_true', help='verify', required=False, default=False)
 
     args = argparser.parse_args()
     session_id = None
